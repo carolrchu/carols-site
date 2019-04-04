3d851813335fc0d75d8ced37fb4ac07a29b69609
Carol Chu <carolchu@Carols-MacBook-Pro-2.local>
2019-04-04 18:23:31 -0400

    init commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+
+
+    <!-- Dynamic Content -->
+    <transition name="fade">
+      <router-view></router-view>
+    </transition>
+
+  </div>
+</template>
+
+<style lang="scss">
+@import 'assets/main.scss';
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
+
+<script lang="ts">
+import Vue from 'vue'
+// @ is an alias to /src
+
+export default Vue.extend({
+  name: 'page1'
+})
+</script>
---
+++ src/views/Page1.vue
@@ -0,0 +1,80 @@
+
+<template>
+  <div class="page1">
+    <ul class="nav justify-content-end">
+  <li class="nav-item">
+    <a class="nav-style" href="#">resume</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-style" href="#">about</a>
+  </li>
+</ul>
+    <div class="container">
+      <div class="row">
+        <div class="col-10 mx-auto text-center">
+            <img src="@/assets/images/background-pic.png" class="img-fluid" alt="background-pic" style="margin-top: 2%">
+            <div style="position: absolute; width: 100%;" class="display:flex; justify-content:center; align-items:center;">
+              <h1 class="heading">Hi, I'm Carol.               <h1 class="heading-italicized">Carol Chu.</h1>
+ </h1>
+
+            </div>
+        </div>
+      </div>
+    </div>
+    <div class="mx-auto text-center">
+      <!-- <h1 class="heading">Hi, I'm Carol.</h1> -->
+      <!-- <h1 class="heading-italicized">Carol Chu.</h1> -->
+    </div>
+
+  <div class="container">
+    <div class="row">
+      <div class="col-12 mx-auto text-center">
+          <img src="@/assets/images/tdlab.png" class="img-fluid" alt="tdlab" style="margin: 10% 0">
+      </div>
+    </div>
+  </div>
+
+  <div class="container">
+    <div class="row">
+      <div class="col-12 mx-auto text-center">
+          <img src="@/assets/images/personalproject.png" class="img-fluid" alt="tdlab" style="margin: 5% 0">
+      </div>
+    </div>
+  </div>
+
+  <div class="container">
+    <div class="row">
+      <div class="col-12 mx-auto text-center">
+          <img src="@/assets/images/grow.png" class="img-fluid" alt="tdlab" style="margin: 5% 0">
+      </div>
+    </div>
+  </div>
+
+  <div class="container">
+    <div class="row">
+      <div class="col-12 mx-auto text-center">
+          <img src="@/assets/images/tablestory.png" class="img-fluid" alt="tdlab" style="margin: 5% 0">
+      </div>
+    </div>
+  </div>
+
+
+
+  </div>
+</template>
+
+<style lang="scss">
+@import url("/@/assets/main.scss");
+</style>
+
+<script lang="ts">
+import Vue from 'vue'
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default Vue.extend({
+  name: 'page1',
+  components: {
+    HelloWorld
+  }
+})
+</script>
